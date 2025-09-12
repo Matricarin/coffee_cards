2903314facb1771c382bc077435c88b803397751
Matricarin <matricarin@gmail.com>
2025-09-12 16:29:35 +0500

    simple dtos was been added

--- src/CoffeeCards.Api/DTOs/Requests/UserRequest.cs
+++ src/CoffeeCards.Api/DTOs/Requests/UserRequest.cs
@@ -2,6 +2,8 @@ namespace CoffeeCards.Api.DTOs.Requests
 {
     public class UserRequest
     {
-        
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
     }
 }
--- src/CoffeeCards.Api/DTOs/Responses/UserResponse.cs
+++ src/CoffeeCards.Api/DTOs/Responses/UserResponse.cs
@@ -2,6 +2,8 @@ namespace CoffeeCards.Api.DTOs.Responses
 {
     public class UserResponse
     {
-        
+        public string Email { get; set; } = null!;
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; } = null!;
     }
 }
