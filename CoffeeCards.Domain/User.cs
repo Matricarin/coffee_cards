ff9a9a75ff2bfb99fa8809526b2d99c5743c6a93
Matricarin <matricarin@gmail.com>
2025-09-12 12:47:43 +0500

    user was been added

---
+++ CoffeeCards.Domain/User.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeCards.Domain
+{
+    public class User
+    {
+        public string Email { get; set; } = null!;
+        public Guid Id { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
