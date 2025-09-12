471a6b4c1b2467689af0d95af7483e38ed7f1b2c
Matricarin <matricarin@gmail.com>
2025-09-12 16:44:04 +0500

    simple test controller was been created

--- src/CoffeeCards.Api/Controllers/UsersController.cs
+++ src/CoffeeCards.Api/Controllers/UsersController.cs
@@ -1,7 +1,61 @@
+using CoffeeCards.Api.DTOs.Requests;
+using CoffeeCards.Api.DTOs.Responses;
+using CoffeeCards.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CoffeeCards.Api.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
-        
+        private static readonly List<User> _users = new();
+
+        public IActionResult Create(UserRequest request)
+        {
+            var user = new User(request.Email, request.PhoneNumber, request.Password); // hashed password
+
+            _users.Add(user);
+
+            var response = new UserResponse()
+            {
+                Email = user.Email.Value,
+                Id = user.Id,
+                PhoneNumber = user.PhoneNumber.Value
+            };
+
+            return CreatedAtAction(nameof(GetById), new { Id = user.Id }, response);
+        }
+
+        public IActionResult GetAll()
+        {
+            var response = _users.Select(u => new UserResponse
+            {
+                Email = u.Email.Value,
+                Id = u.Id,
+                PhoneNumber = u.PhoneNumber.Value
+            });
+
+            return Ok(response);
+        }
+
+        public IActionResult GetById(Guid id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var response = new UserResponse
+            {
+                Email = user.Email.Value,
+                Id = user.Id,
+                PhoneNumber = user.PhoneNumber.Value
+            };
+
+            return Ok(response);
+        }
     }
 }
