23492af64f7f26456ad52c578282d41b82b15f2d
Matricarin <matricarin@gmail.com>
2025-09-12 12:56:01 +0500

    address was been added

---
+++ CoffeeCards.Domain/Address.cs
@@ -0,0 +1,18 @@
+namespace CoffeeCards.Domain
+{
+    public class Address 
+    { 
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+        
+        public Address(string street, string city, string state, string zipcode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+        }
+    }
+}
