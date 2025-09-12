3817c2db8fb74cd655734f308e3568de3bd1c39c
Matricarin <matricarin@gmail.com>
2025-09-12 14:14:42 +0500

    models was been updated

--- src/CoffeeCards.Domain/SeedWork/Email.cs
+++ src/CoffeeCards.Domain/SeedWork/Email.cs
@@ -1,7 +1,19 @@
+using System.Text.RegularExpressions;
+
 namespace CoffeeCards.Domain.SeedWork
 {
     public class Email
     {
-        
+        public string Value { get; }
+
+        public Email(string email)
+        {
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                throw new ArgumentException("Invalid email");
+            }
+
+            Value = email;
+        }
     }
 }
