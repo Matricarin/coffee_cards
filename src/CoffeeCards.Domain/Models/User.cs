99a3e0683f74aebea0c7cf3e81f245225d9af407
matricarin <matricarin@gmail.com>
2025-09-21 07:19:10 +0500

    model was fixed

--- src/CoffeeCards.Domain/Models/User.cs
+++ src/CoffeeCards.Domain/Models/User.cs
@@ -4,15 +4,13 @@ namespace CoffeeCards.Domain.Models
 {
     public class User
     {
-        public Email Email { get; private set; }
         public Guid Id { get; private set; }
         public string Password { get; private set; }
         public PhoneNumber PhoneNumber { get; private set; }
 
-        public User(string email, string phone, string password)
+        public User(string phone, string password)
         {
             Id = Guid.NewGuid();
-            Email = new Email(email);
             PhoneNumber = new PhoneNumber(phone);
             Password = password;
         }
