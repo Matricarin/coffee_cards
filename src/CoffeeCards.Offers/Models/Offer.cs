06b2f5f8d4383404226f66521adde90ffc7e8282
matricarin <matricarin@gmail.com>
2025-09-28 19:48:08 +0500

    bsons was been added

--- src/CoffeeCards.Offers/Models/Offer.cs
+++ src/CoffeeCards.Offers/Models/Offer.cs
@@ -1,12 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace CoffeeCards.Offers.Models
 {
     public class Offer
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public required string Id { get; set; }
         public Guid CoffeeShopId { get; set; }
         public required string Title { get; set; }
         public required string Type { get; set; }
-        public required string Parameters { get; set; }
+        public required BsonDocument Parameters { get; set; }
         public DateTime? ValidFrom { get; set; }
         public DateTime? ValidUntil { get; set; }
     }
