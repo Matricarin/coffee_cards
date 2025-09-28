3a234744e3cc803b02e6f359735e05b9b34fa0ff
matricarin <matricarin@gmail.com>
2025-09-28 20:29:44 +0500

    repository was been added

--- src/CoffeeCards.Core/Interfaces/ICardRepository.cs
+++ src/CoffeeCards.Core/Interfaces/ICardRepository.cs
@@ -5,6 +5,7 @@ namespace CoffeeCards.Core.Interfaces
     public interface ICardRepository
     {
         Task<IReadOnlyList<Card>> GetUserCardsAsync(Guid userId);
+        Task<Card> GetCardByIdAsync(Guid cardId);
         void AddCard(Card card);
         void DeleteCard(Card card);
         void UpdateCard(Card card);
