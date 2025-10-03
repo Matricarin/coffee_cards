0838ab2b49f55ba6f46e21dc67cd101c8ad7366d
Matricarin <matricarin@gmail.com>
2025-10-03 07:55:06 +0500

    removed

---
+++ src/CoffeeCards.Core/Entities/Offer.cs
@@ -0,0 +1,12 @@
+namespace CoffeeCards.Core.Entities
+{
+    public class Offer
+    {
+        public required string Id { get; set; }
+        public Guid CoffeeShopId { get; set; }
+        public required string Title { get; set; }
+        public required string Type { get; set; }
+        public DateTime? ValidFrom { get; set; }
+        public DateTime? ValidUntil { get; set; }
+    }
+}
