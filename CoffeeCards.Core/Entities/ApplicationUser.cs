7d491ca6da79797482ba3d285c2230a5f6feb4a9
matricarin <matricarin@gmail.com>
2025-12-12 06:45:09 +0500

    properties for user were added

--- CoffeeCards.Core/Entities/ApplicationUser.cs
+++ CoffeeCards.Core/Entities/ApplicationUser.cs
@@ -5,6 +5,14 @@ namespace CoffeeCards.Core.Entities
 {
     public sealed class ApplicationUser : IdentityUser<Guid>
     {
+        /// <summary>
+        /// Name for display in client app 
+        /// </summary>
+        public string? DisplayName {get; set;}
         
+        /// <summary>
+        /// Name if user is admin in coffee shop
+        /// </summary>
+        public string? ShopName {get; set;}
     }
 }
